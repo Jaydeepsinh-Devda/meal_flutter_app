26dda433f2b3f08c3afc9d609bad1cc0b5d39f4e
Jaydeepsinh Devda <djaybhai76@gmail.com>
2023-11-17 11:24:36 +0530

    Added a Side Drawer

--- lib/Screens/tabs_screen.dart
+++ lib/Screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/Models/meal_model.dart';
 import 'package:meals_app/Screens/category_screen.dart';
 import 'package:meals_app/Screens/meals_screen.dart';
+import 'package:meals_app/Widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -67,13 +68,13 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
+      drawer: MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         currentIndex: _selectedPageIndex,
         items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.set_meal), label: 'Categories'),
+          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites')
         ],
       ),
